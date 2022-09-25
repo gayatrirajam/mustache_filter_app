08b0e95dd5e93ebbf6285c65715aab71e3c67ec0
gayatrirajam <96321688+gayatrirajam@users.noreply.github.com>
2022-09-25 11:47:09 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,33 @@
+noseX = 0;
+noseY = 0;
 function preload(){
 
 }
 function setup(){
     canvas=createCanvas(650, 600);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(650, 600)
+    video.hide();
+
+    poseNet=ml5.poseNet(video,modelLoaded)
+    poseNet.on('pose', gotPoses)
 }
 function draw(){
-    
+
 }
 function save(){
 
+}
+function modelLoaded(){
+    console.log("PoseNet is loaded!");
+}
+function gotPoses(){
+    if(results.length > 0){
+        console.log(results);
+        noseX=results[0].pose.nose.x;
+        noseY=results[0].pose.nose.y;
+        console.log("noseX = " + noseX);
+        console.log("noseY = " + noseY);
+    }
 }
