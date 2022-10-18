d10546a6776edeac656068f4e8913a7d71ad0e9e
gayatrirajam <96321688+gayatrirajam@users.noreply.github.com>
2022-10-18 18:54:06 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,33 +1,34 @@
-noseX = 0;
-noseY = 0;
+noseX=0;
+noseY=0;
 function preload(){
-
+    link = loadImage('https://th.bing.com/th/id/R.ab1ab4c8e3cc78f815893d48495aeacd?rik=MS5ExVH53%2byHsg&riu=http%3a%2f%2fclipart-library.com%2fimages_k%2fmustache-transparent-png%2fmustache-transparent-png-4.png&ehk=ykTdznjWeuA33ao0NePZFjkycKrlN2LSK9bRmtxU2qo%3d&risl=&pid=ImgRaw&r=0');
 }
 function setup(){
-    canvas=createCanvas(650, 600);
+    canvas = createCanvas(300, 300);
     canvas.center();
     video=createCapture(VIDEO);
-    video.size(650, 600)
+    video.size(300, 300);
     video.hide();
 
-    poseNet=ml5.poseNet(video,modelLoaded)
-    poseNet.on('pose', gotPoses)
+    poseNet=ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 function draw(){
-
-}
-function save(){
-
+    image(video, 0, 0, 300, 300);
+    image(link, noseX, noseY, 50, 30);
 }
 function modelLoaded(){
-    console.log("PoseNet is loaded!");
+    console.log("Posenet is loaded");
 }
-function gotPoses(){
+function gotPoses(results){
     if(results.length > 0){
         console.log(results);
-        noseX=results[0].pose.nose.x;
-        noseY=results[0].pose.nose.y;
+        noseX=results[0].pose.nose.x - 25;
+        noseY=results[0].pose.nose.y + 3;
         console.log("noseX = " + noseX);
         console.log("noseY = " + noseY);
     }
+}
+function take_snapshot(){
+    save('myMustacheFilterImage.png');
 }
